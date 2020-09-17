d7a90aae3dfb60edabf362a0e4b25144a4eee8c3
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-09-17 19:27:13 +0200

    refactor: move all the shell code to javascript

--- index.js
+++ index.js
@@ -4,4 +4,34 @@ const core = require('@actions/core');
 let forAllModules = core.getInput('for-all-modules');
 let forEachModule = core.getInput('for-each-module');
 
-shell.exec('./entrypoint.sh ' + forEachModule + ' ' + forAllModules);
+// unshallow since GitHub actions does a shallow clone
+shell.exec('git fetch --unshallow');
+shell.exec('git fetch origin');
+
+const baseBranch = 'origin/' + process.env.GITHUB_BASE_REF;
+
+let changed_modules = [];
+shell.exec('git diff --name-only ' + baseBranch, {silent:true}, function (code, stdout, stderr) {
+    let changedFiles = stdout.split(/\r?\n/);
+    changedFiles.forEach(function(fileName) {
+        let moduleName = fileName.split('/')[0];
+        if (!changed_modules.includes(moduleName)) {
+            changed_modules.push(moduleName);
+        }
+    });
+
+    let availableTasks = shell.exec('./gradlew tasks --all', {silent:true}).stdout;
+
+    let buildCommands = "";
+    changed_modules.forEach(function (module) {
+        if (availableTasks.includes(module + ':app:')) {
+            if (module != '') {
+                buildCommands += " :" + module + ":app:" + forEachModule +
+                    " :" + module + ":app:check";
+            }
+        }
+    });
+
+    console.log('Building Pull Request with:', buildCommands);
+    shell.exec('./gradlew ' + forAllModules + ' ' + buildCommands);
+});
